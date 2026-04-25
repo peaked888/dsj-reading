02ec1281b9957b7f957957266bbfd55aa4d1ad2b
peaked888 <peaked888@gmail.com>
2026-04-26 01:15:18 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,12 +1,11 @@
-// 極簡 Service Worker - 只讓 PWA 可以安裝，不做快取
-const CACHE = 'dsj-v2';
+// v20260426 - 強制清除所有快取
+const CACHE = 'dsj-v20260426';
 
 self.addEventListener('install', e => {
   self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
-  // 清除所有舊快取
   e.waitUntil(
     caches.keys().then(keys =>
       Promise.all(keys.map(k => caches.delete(k)))
@@ -14,7 +13,6 @@ self.addEventListener('activate', e => {
   );
 });
 
-// 所有請求直接走網路，不快取
 self.addEventListener('fetch', e => {
   e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
