16a64a19c07c5739be4302ca7e1171e1479ef1b9
peaked888 <peaked888@gmail.com>
2026-07-14 01:00:02 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// update-v1.1-20260714-submit-fix - 修正手機送出與離線備援，強制淘汰舊快取
-const CACHE = 'dsj-update-v1.1-20260714-submit-fix';
+// update-v1.1-20260714-submit-fix2 - 還原相容寫入流程，強制淘汰舊快取
+const CACHE = 'dsj-update-v1.1-20260714-submit-fix2';
 
 self.addEventListener('install', e => {
   self.skipWaiting();
